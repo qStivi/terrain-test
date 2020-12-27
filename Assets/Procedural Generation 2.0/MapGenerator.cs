b78728740aee5006483ff49ac7a2bb4d5baa916e
qStivi <stephanglaue@yahoo.de>
2020-12-27 19:37:38 +0100

    Stuff

--- Assets/Procedural Generation 2.0/MapGenerator.cs
+++ Assets/Procedural Generation 2.0/MapGenerator.cs
@@ -7,13 +7,30 @@ namespace Procedural_Generation_2._0
     {
         public int mapWidth = 100;
         public int mapHeight = 100;
-        public float noiseScale = .3f;
+        public float noiseScale = 25f;
+
+        [Range(1, 30)] public int octaves = 4;
+
+        [Range(0, 1)] public float persistence = 0.5f;
+
+        public float lacunarity = 2f;
+        public int seed;
+        public Vector2 offset;
 
         public bool autoUpdate = true;
 
+        private void OnValidate()
+        {
+            if (mapWidth < 1) mapWidth = 1;
+
+            if (mapHeight < 1) mapHeight = 1;
+
+            if (lacunarity < 1) lacunarity = 1;
+        }
+
         public void GenerateMap()
         {
-            var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale); // Generate noise map.
+            var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, seed, octaves, persistence, lacunarity, offset); // Generate noise map.
 
             // Visualize noise map.
             var display = GetComponent<MapDisplay>();
--- Assets/Procedural Generation 2.0/Noise.cs
+++ Assets/Procedural Generation 2.0/Noise.cs
@@ -1,27 +1,70 @@
 using UnityEngine;
+using Random = System.Random;
 
 namespace Procedural_Generation_2._0
 {
     public static class Noise
     {
         // This method creates a two dimensional float array and assigns pseudo random values to it using perlin noise.
-        public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+        public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int seed, int octaves, float persistence, float lacunarity, Vector2 offset)
         {
             var noiseMap = new float[mapWidth, mapHeight];
 
+            // Create a pseudo random values while also concerning a custom offset.
+            // With this we have a random offset which is decided by a seed and a manuel offset. We have randomness by offsetting.
+            var prng = new Random(seed); // Create new instance of random generator with seed so it works like minecraft seeds.
+            var octaveOffset = new Vector2[octaves];
+            for (var o = 0; o < octaves; o++)
+            {
+                // Larger ranges tend to produce repeating values. 
+                var offsetX = prng.Next(-100000, 100000) + offset.x;
+                var offsetY = prng.Next(-100000, 100000) + offset.y;
+                octaveOffset[o] = new Vector2(offsetX, offsetY);
+            }
+
             if (scale == 0) scale = 0.00001f; // prevent division by 0.
+
+            // Using this no normalize noise map after generating.
+            var maxNoiseHeight = float.MinValue;
+            var minNoiseHeight = float.MaxValue;
+
+            // Using this to "zoom" to the center instead of top right.
+            var halfWidth = mapWidth / 2f;
+            var halfHeight = mapHeight / 2f;
 
             for (var y = 0; y < mapHeight; y++)
             for (var x = 0; x < mapWidth; x++)
             {
-                // apply scale factor.
-                var sampleX = x / scale;
-                var sampleY = y / scale;
+                var amplitude = 1f;
+                var frequency = 1f;
+                var noiseHeight = 0f; // Current height value
 
-                // calculate and store perlin value.
-                var perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinValue;
+                for (var o = 0; o < octaves; o++)
+                {
+                    // apply scale factor and frequency.
+                    var sampleX = (x - halfWidth) / scale * frequency + octaveOffset[o].x;
+                    var sampleY = (y - halfHeight) / scale * frequency + octaveOffset[o].y;
+
+                    // calculate and store noiseHeight.
+                    var perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;  // Calculate perlinValue for our current "pixel". Then change range to -1 to 1 for more interesting values.
+                                                                                        // Mathf.PerlinNoise() returns a value between 0 and 1 by default.
+                    noiseHeight += perlinValue * amplitude; // Apply amplitude.
+
+                    amplitude *= persistence;
+                    frequency *= lacunarity;
+                }
+
+                // Set min and max noiseHeight.
+                if (noiseHeight > maxNoiseHeight) maxNoiseHeight = noiseHeight;
+                else if (noiseHeight < minNoiseHeight) minNoiseHeight = noiseHeight;
+
+                noiseMap[x, y] = noiseHeight;
             }
+
+            // Normalize noise map. (Fit values to range 0 to 1)
+            for (var y = 0; y < mapHeight; y++)
+            for (var x = 0; x < mapWidth; x++)
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
 
             return noiseMap;
         }
