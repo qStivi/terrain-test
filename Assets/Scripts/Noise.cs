0adecb117b8341134b51fae2cb9accc51c76e09d
qStivi <stephanglaue@yahoo.de>
2021-01-28 12:52:35 +0100

    Stuff

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -13,20 +13,20 @@ public static class Noise
     // This method creates a two dimensional float array and assigns pseudo random values to it using perlin noise.
     public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, NoiseSettings settings, Vector2 sampleCentre)
     {
-        if (settings.randomSeed) settings.seed = new Random().Next(int.MinValue, int.MaxValue);
+        if (settings.randomSeed) settings.seed = new Random().Next(int.MinValue, int.MaxValue); // randomize seed if desired.
 
-        // This is our "main variable". An 2d array of height values which can be represented as a map.
-        // For each pair of values or coordinates a height value will be calculated using perlin noise, random offsets, octaves, a seed, a persistence value and a lacunarity value.
+        // This is our "main variable". A 2d array of height values which can be represented as a map.
+        // For each pair of values (or coordinates) a height value will be calculated using perlin noise, random offsets, octaves, a seed, a persistence value and a lacunarity value.
         var noiseMap = new float[mapWidth, mapHeight];
 
-        // Create a pseudo random values. Pseudo: Because we can use a seed to always get the same values.
-        // With this we have a random offset which is decided by a seed and a manuel offset.
+        // Create a pseudo random value.
         var prng = new Random(settings.seed); // Create new instance of random generator with seed so it works like minecraft seeds.
-        var octaveOffset = new Vector2[settings.octaves]; // Octaves are multiple stacked layers of noise with different resolutions
+        var octaveOffset = new Vector2[settings.octaves]; // Octaves are stacked layers of noise with different resolutions
 
-        var maxPossibleHeight = 0f;
+        var maxPossibleHeight = 0f; // This will be changed every octave using the amplitude value.
         var amplitude = 1f; // This will be changed every octave using the persistence value.
 
+        // calculating octaves
         for (var i = 0; i < settings.octaves; i++)
         {
             // Larger ranges tend to produce repeating values. 
@@ -34,7 +34,7 @@ public static class Noise
             var offsetY = prng.Next(-100000, 100000) - settings.offset.y - sampleCentre.y;
             octaveOffset[i] = new Vector2(offsetX, offsetY);
 
-            maxPossibleHeight += amplitude;
+            maxPossibleHeight += amplitude; // Gets larger each octave.
             amplitude *= settings.persistence; // Gets smaller each octave.
         }
 
@@ -46,6 +46,7 @@ public static class Noise
         var halfWidth = mapWidth / 2f;
         var halfHeight = mapHeight / 2f;
 
+        // calculating actual noise
         for (var y = 0; y < mapHeight; y++)
         for (var x = 0; x < mapWidth; x++)
         {
@@ -58,13 +59,12 @@ public static class Noise
                 // apply scale factor, frequency and random offset.
                 var sampleX = (x - halfWidth + octaveOffset[i].x) / settings.scale * frequency;
                 var sampleY = (y - halfHeight + octaveOffset[i].y) / settings.scale * frequency;
-
-                // calculate noiseHeight.
-
+                
                 // Calculate perlinValue for our current "pixel". Then change range to -1 to 1 for more interesting values.
                 // Mathf.PerlinNoise() returns a value between 0 and 1 by default.
                 var perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
 
+                // calculate noiseHeight.
                 noiseHeight += perlinValue * amplitude; // Apply amplitude.
 
                 amplitude *= settings.persistence; // Gets smaller each octave.
@@ -75,17 +75,17 @@ public static class Noise
             if (noiseHeight > maxLocalNoiseHeight) maxLocalNoiseHeight = noiseHeight;
             if (noiseHeight < minLocalNoiseHeight) minLocalNoiseHeight = noiseHeight;
 
-            noiseMap[x, y] = noiseHeight; // Set height value for current coordinate.
+            noiseMap[x, y] = noiseHeight; // Save calculated height in our map
 
-            if (settings.normalizeMode == NormalizeMode.Global)
-            {
-                var normalizedHeight = (noiseMap[x, y] + 1) / maxPossibleHeight / 0.9f;
-                noiseMap[x, y] = Mathf.Clamp(normalizedHeight, 0, int.MaxValue);
-            }
+            // normalize calculated value according to normalize mode.
+            if (settings.normalizeMode != NormalizeMode.Global) continue;
+            var normalizedHeight = (noiseMap[x, y] + 1) / maxPossibleHeight / 0.9f;
+            noiseMap[x, y] = Mathf.Clamp(normalizedHeight, 0, int.MaxValue);
         }
 
+        // normalize calculated value according to normalize mode.
+        // ReSharper disable once InvertIf
         if (settings.normalizeMode == NormalizeMode.Local)
-            // Normalize noise map. (Fit values to range 0 to 1)
             for (var y = 0; y < mapHeight; y++)
             for (var x = 0; x < mapWidth; x++)
                 noiseMap[x, y] = Mathf.InverseLerp(minLocalNoiseHeight, maxLocalNoiseHeight, noiseMap[x, y]);
